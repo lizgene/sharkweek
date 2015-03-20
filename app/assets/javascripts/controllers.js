65f19c4e9c1923d7ac547e6a9706b79f7305765c
Liz Hubertz <liz.hubertz@gmail.com>
2015-03-20 12:50:44 +0700

    search function

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -2,16 +2,42 @@
 
 var sharkweekControllers = angular.module('sharkweekControllers', [])
 
-sharkweekControllers.controller('HomeCtrl', ['$scope',
-  function($scope) {
-	  $scope.products = [
-	    {'name': 'Tampax tampon',
-	     'snippet': 'THE NEXT GEN.'},
-	    {'name': 'Kotex tampon',
-	     'snippet': 'This is awesome.'},
-	    {'name': 'Random pad',
-	     'snippet': 'This one will probably kill you.'}
-	  ];
+sharkweekControllers.controller('HomeCtrl', ['$scope', '$routeParams', '$location',
+  function($scope, $routeParams, $location) {
+
+  	var products = [
+		  {
+		    "id": "1",
+		    "name": 'Baked Potato w/ Cheese'
+		  },
+		  {
+		    "id": "2",
+		    "name": 'Garlic Mashed Potatoes',
+		  },
+		  {
+		    "id": "3",
+		    "name": 'Potatoes Au Gratin',
+		  },
+		  {
+		    "id": "4",
+		    "name": 'Baked Brussel Sprouts',
+		  },
+		]
+
+
+  	var keywords;
+  	$scope.search = function(keywords){
+  		return $location.path('/').search('keywords', keywords)
+  	}
+	  
+  	if($routeParams.keywords){
+  		keywords = $routeParams.keywords.toLowerCase();
+  		$scope.products = products.filter(function(product) {
+        return product.name.toLowerCase().indexOf(keywords) !== -1;
+      });
+    } else {
+      return $scope.products = [];
+    }
   }]);
 
 sharkweekControllers.controller('AboutCtrl', ['$scope',
